89f0453728db7ff5b525af491d79a16c835bff4c
meeramca <94525942+meeramca@users.noreply.github.com>
2022-04-19 16:09:23 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,28 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package employee;
 
 public class Employee {
-    int employee_id;
-    String employee_name;
-    int salary;
-    Employee(int a, String b,int c)
-    {
-        employee_id = a;
-        employee_name = b;
-        salary = c;
-    }
-    public void tot_salary(){
-        int tot_salary = salary+(salary*20/100);
-        System.out.println("total salary with DA 20% is " +tot_salary );
-    }
+    int id;
+	String name;
+	
+	public Employee(int i, String n)
+	{
+		id = i;
+		name = n;
+	}
+	void show()
+	{
+		System.out.println(id+" "+name);
+	}
     public static void main(String[] args) {
-        Employee e = new Employee(101, "joy", 3000);
-        e.tot_salary();
-        
+        Employee emp1 = new Employee(1, "Govind");
+        Employee emp2 = new Employee(2, "Akash");
+	emp1.show();
+	emp2.show(); 
     }
     
 }
